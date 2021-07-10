82c7730523ce60043f0da9fb654779a252a25d75
Diego Kiedanski <dkiedanski@gmail.com>
2021-07-09 21:40:44 -0300

    recovering out folder

--- utils/run_pandoc.sh
+++ utils/run_pandoc.sh
@@ -5,5 +5,8 @@ name=$(basename $1)
 name="${name%%.*}"
 path=$(dirname $1)
 
+outdir="$path/out"
+mkdir -p "$outdir"
 
-pandoc -d utils/params.yaml -o "$path/$name.html" "$1"
+
+pandoc -d utils/params.yaml -o "$outdir/$name.html" "$1"
